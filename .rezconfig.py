c8de31bb673dadb5928e340344723c97213883d1
Jason Scott <jason@thescottclan.org>
2019-01-26 09:03:11 +1030

    Some Windows changes

--- .rezconfig.py
+++ .rezconfig.py
@@ -3,6 +3,11 @@ package_filter = dict(
     excludes = [
         "*.dev",
         "*.dev.*"
-#        "*.encore.*"
     ]
 )
+packages_path = [
+    'Z:/Applications_and_Programs/rez/core',
+    'Z:/Applications_and_Programs/rez/pip',
+    'Z:/Applications_and_Programs/rez/third-party',
+]
+release_packages_path = 'Z:/Applications_and_Programs/rez/core'
